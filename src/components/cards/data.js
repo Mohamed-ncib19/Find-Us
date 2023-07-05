8db7fa627f5e2e54767a81030b613d0f67cb4b1c
Mohamed-ncib19 <mohamedncib900@gmail.com>
2023-07-05 17:50:24 +0100

    modify pictures path

--- src/components/cards/data.js
+++ src/components/cards/data.js
@@ -1,20 +1,29 @@
+import FirebasePicture from "../../assets/firebase.png"
+import HTMLPicture from "../../assets/html.png"
+import MEPicture from "../../assets/ME.png"
+import PythonPicture from "../../assets/python.png"
+import ReactPicture from "../../assets/react.png"
+import TailwindPicture from "../../assets/tailwind.png"
+import VitePicture from "../../assets/vite.png"
+import WpPicture from "../../assets/wp.png"
+
 const MyCards =[
-    {id:1 , src:"../src/assets/firebase.png" ,check:"",index:0},
-    {id:1 , src:"../src/assets/firebase.png",check:"",index:1},
-    {id:2 , src:"../src/assets/html.png",check:"",index:2},
-    {id:2 , src:"../src/assets/html.png",check:"",index:3},
-    {id:3 , src:"../src/assets/ME.png",check:"",index:4},
-    {id:3 , src:"../src/assets/ME.png",check:"",index:5},
-    {id:4 , src:"../src/assets/python.png",check:"",index:6},
-    {id:4 , src:"../src/assets/python.png",check:"",index:7},
-    {id:5 , src:"../src/assets/react.png",check:"",index:8},
-    {id:5 , src:"../src/assets/react.png",check:"",index:9},
-    {id:6 , src:"../src/assets/tailwind.png",check:"",index:10},
-    {id:6 , src:"../src/assets/tailwind.png",check:"",index:11},
-    {id:7 , src:"../src/assets/vite.png",check:"",index:12},
-    {id:7 , src:"../src/assets/vite.png",check:"",index:13},
-    {id:8 , src:"../src/assets/wp.png",check:"",index:14},
-    {id:8 , src:"../src/assets/wp.png",check:"",index:15},
+    {id:1 , src:FirebasePicture ,check:"",index:0},
+    {id:1 , src:FirebasePicture,check:"",index:1},
+    {id:2 , src:HTMLPicture,check:"",index:2},
+    {id:2 , src:HTMLPicture,check:"",index:3},
+    {id:3 , src:MEPicture,check:"",index:4},
+    {id:3 , src:MEPicture,check:"",index:5},
+    {id:4 , src:PythonPicture,check:"",index:6},
+    {id:4 , src:PythonPicture,check:"",index:7},
+    {id:5 , src:ReactPicture,check:"",index:8},
+    {id:5 , src:ReactPicture,check:"",index:9},
+    {id:6 , src:TailwindPicture,check:"",index:10},
+    {id:6 , src:TailwindPicture,check:"",index:11},
+    {id:7 , src:VitePicture,check:"",index:12},
+    {id:7 , src:VitePicture,check:"",index:13},
+    {id:8 , src:WpPicture,check:"",index:14},
+    {id:8 , src:WpPicture,check:"",index:15},
 ].sort(()=>Math.random()-0.5);
 
 export default MyCards;
